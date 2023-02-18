c35f50bc93f3c89925fc6d8dd0b73fdb3bb36ba0
Nikolai Plegunov <plegunovn@gmail.com>
2023-02-18 14:05:47 +0300

    first_task

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!usr/bin/bash
+cd ~/test_box/33Group/
+mkdir sk_1 sk_2 sk_3
+cd ~/test_box/33Group/sk_2/
+touch 1.txt 2.txt 3.txt 4.json 5.json
+mkdir n_1 n_2 n_3
+mv ~/test_box/33Group/sk_2/1.txt ~/test_box/33Group/sk_1/1.txt
+mv ~/test_box/33Group/sk_2/2.txt ~/test_box/33Group/sk_2/n_2/2.txt
+
